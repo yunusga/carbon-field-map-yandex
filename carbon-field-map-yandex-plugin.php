39657cd54809bd7188b42689cf04423b211e507f
Yunus Gaziev <yunusga@yandex.ru>
2022-06-25 11:00:11 +0500

    feat: Add WordPress plugin support

---
+++ carbon-field-map-yandex-plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Carbon field extension Map Yandex for Carbon Fields
+ *
+ * @package carbon-field-map-yandex
+ * @link    https://github.com/yunusga/carbon-field-map-yandex
+ * @author  Yunus Gaziev <yunusga@yunusga.uz>
+ * @license GPL2
+ *
+ * Plugin Name:       Carbon Field Map Yandex
+ * Description:       Carbon Fields extension, that adds a Map Yandex field type.
+ * Version:           1.0.0
+ * Plugin URI:        https://github.com/yunusga/carbon-field-map-yandex
+ * Author:            Yunus Gaziev
+ * Author URI:        https://yunusga.uz/
+ * License:           GPL2
+ * Text Domain:       carbon-field-map-yandex
+ * Domain Path:       /languages
+ * Requires at least: 5.0.0
+ * Tested up to:      6.0.0
+ */
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require __DIR__ . '/vendor/autoload.php';
+}
--- field.php
+++ field.php
@@ -5,13 +5,16 @@ use Carbon_Field_Map_Yandex\Map_Yandex_Field;
 
 define( 'Carbon_Field_Map_Yandex\\DIR', __DIR__ );
 
-Carbon_Fields::extend(
-	Map_Yandex_Field::class,
-	function( $container ) {
-		return new Map_Yandex_Field(
-			$container['arguments']['type'],
-			$container['arguments']['name'],
-			$container['arguments']['label']
+function carbon_fields_extend_with_map_yandex_field() {
+		Carbon_Fields::extend(
+			Map_Yandex_Field::class,
+			function( $container ) {
+				return new Map_Yandex_Field(
+					$container['arguments']['type'],
+					$container['arguments']['name'],
+					$container['arguments']['label']
+				);
+			}
 		);
-	}
-);
+}
+add_action( 'carbon_fields_register_fields', 'carbon_fields_extend_with_map_yandex_field', 10 );
